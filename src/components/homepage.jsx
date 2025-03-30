3b7eac815da11d6c11464f861dc5791826955709
Sachin Vardhan <vardhansachin08@gmail.com>
2025-03-30 09:53:15 +0530

    added production backend url

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -122,9 +122,9 @@ function Homepage() {
 
   return (
     <nav>
-      <div className="max-w-full mx-auto px-4 sm:px-6 lg:px-6 font-inter transition-colors duration-200">
-        <div className="relative min-h-[100vh] w-full overflow-hidden rounded-xl mt-28 sm:mt-28">
-          <div className="absolute inset-0 z-10 bg-black/50" />
+      <div className="max-w-full mx-auto sm:px-6 lg:px-6 font-inter transition-colors duration-200">
+        <div className="relative min-h-[100vh] w-full overflow-hidden rounded-xl mt-28 sm:mt-28 px-4">
+          <div className="absolute inset-0 z-10 bg-black/50 p-x" />
           <video
             className="absolute inset-0 w-full h-full object-cover rounded-xl"
             autoPlay
